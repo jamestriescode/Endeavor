e7dba2b61b2667dacfb750b44684802d2623712d
sargon <jamesbcarlile@gmail.com>
2023-12-21 19:51:21 -0600

    Dabbled with text editor install

--- Setup/Tools/runner.sh
+++ Setup/Tools/runner.sh
@@ -25,7 +25,6 @@ mkdir ~/GitHub
 sudo cp /home/sargon/Endeavor/Setup/Fonts/PCBius.ttf /usr/share/fonts
 cp /home/sargon/Endeavor/Setup/.zshrc /home/sargon/.zshrc
 cp -r /home/sargon/Endeavor/Pictures /home/sargon
-cp -r /home/sargon/Endeavor/Setup/nvim /home/sargpn/.config/
 mv ~/Endeavor ~/Github/Endeavor
 
 # Execute refresh.sh
@@ -52,7 +51,6 @@ lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader l
 
 # Clone git packages
 git clone https://github.com/sargoncodes/Projects ~/GitHub/
-git clone https://github.com/arkenfox/user.j ~/.mozilla/firefox/
 cp -r user.js/* ~/.mozilla/firefox/*.default-release
 rm -rf ~/.mozilla/firefox/user.js
 
